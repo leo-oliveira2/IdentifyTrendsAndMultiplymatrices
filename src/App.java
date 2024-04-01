f790c1e15a39d1a365e2b6da773221afe9be396a
leo-oliveira2 <heinen126@gmail.com>
2024-04-01 20:32:58 -0300

    Start

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public boolean hasTrend(String[] S, String[] S_line){
+        return false;
+    }
+
+    public int[][] multiply(int[][] A, int[][] B){
+        int[][] retorno = new int[10][10];
+        return retorno;
+    }
+}
